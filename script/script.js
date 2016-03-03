b6f61293508983a5f93d20827826a0064f3234f7
Arnie <arnie@templatemonster.me>
2016-03-03 15:33:43 +0200

    Добавил замеры скорости с регексом

--- script/script.js
+++ script/script.js
@@ -22,18 +22,37 @@ var telInputHandler = (function(){
         }
     }
 
+    function _separateValueWithRegex(){
+        rawValue = rawValue.trim();
+        var len = rawValue.length;
+
+        switch (len) {
+            case 6 : separatedValue = rawValue.replace(/(\d{2})(\d{2})(\d{2})/, "$1 $2 $3"); //'00 00 00';
+                break;
+            case 7 : separatedValue = rawValue.replace(/(\d{3})(\d{2})(\d{2})/, "$1 $2 $3"); //'000 00 00';
+                break;
+            case 8 : separatedValue = rawValue.replace(/(\d{3})(\d{3})(\d{2})/, "$1 $2 $3"); //'000 000 00';
+                break
+            case 9 : separatedValue = rawValue.replace(/(\d{3})(\d{3})(\d{3})/, "$1 $2 $3"); //'000 000 000';
+                break;
+            case 10 : separatedValue = rawValue.replace(/(\d{3})(\d{3})(\d{2})(\d{2})/, "$1 $2 $3 $4"); //'000 000 00 00';
+                break;
+            default : separatedValue = rawValue;
+        }
+    }
+
     function _getBuckets(mask){
         var tmp = [],
             maskLen = mask.length,
             i = 0,
             j = 0;
 
-        for(; i < maskLen; i += 1 ){
+        for(; i < maskLen; i = i + 1 ){
             if (mask[i] != ' ') {
-                tmp.push(rawValue[j]);
-                j += 1;
+                tmp[i] = rawValue[j];
+                j = j + 1;
             }else{
-                tmp.push(' ');
+                tmp[i] = ' ';
             }
         }
 
@@ -52,11 +71,20 @@ var telInputHandler = (function(){
         var newValue = $(this).val().trim();
         if (newValue != rawValue) {
             rawValue = newValue;
-            t1 = performance.now();
-            _separateValue();
-            t2 = performance.now();
+            var o = 100000, z= 0, p=0;
+            while(o--){
+                r1 = performance.now();
+                _separateValue();
+                r2 = performance.now();
+                p += +(r2-r1).toFixed(6);
 
-            console.log((t2-t1).toFixed(4));
+                t1 = performance.now();
+                _separateValueWithRegex();
+                t2 = performance.now();
+                z += +(t2-t1).toFixed(6);
+            }
+            console.log('Без регекса', p/100000);
+            console.log('C регексом', z/100000);
         }
 
         _fillSeparated();
